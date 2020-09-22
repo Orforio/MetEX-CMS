7fde2b5ce7be6ae3083c8afc4efd2a36fed1343d
Richard Whittaker <pkeruno@gmail.com>
2020-09-22 20:06:30 +0000

    Add stationBySlug

---
+++ api/station/config/schema.graphql.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const { sanitizeEntity } = require('strapi-utils');
+
+module.exports = {
+	query: `stationBySlug(lineSlug: String!, stationSlug: String!): Station`,
+	resolver: {
+		Query: {
+			stationBySlug: {
+				description: 'Return a single Station by Line and slug',
+				resolverOf: 'Station.findOne',
+				async resolver(_, { lineSlug, stationSlug }) {
+					const entity = await strapi.services.station.findOne({
+						slug: stationSlug,
+						_where: {
+							'line.slug': lineSlug
+						}
+					});
+					return sanitizeEntity(entity, { model: strapi.models.station });
+				}
+			}
+		}
+	}
+};
